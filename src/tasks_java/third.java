0182c1e9b233bcf2ca5d07d7580e8ec0a3653ece
nastenka-28-10 <nastenka28.10.96@gmail.com>
2017-10-08 15:34:59 +0300

    not useful

--- src/tasks_java/third.java
+++ src/tasks_java/third.java
@@ -2,8 +2,6 @@ package tasks_java;
 
 import java.lang.Math;
 import java.util.Scanner;
-import java.util.regex.Matcher;  
-import java.util.regex.Pattern;
 
 public class third {
 	public static void main(String args[]){
