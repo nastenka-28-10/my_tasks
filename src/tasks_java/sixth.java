d3bd37492f188263d7797081e3d8529ad9063227
nastenka-28-10 <nastenka28.10.96@gmail.com>
2017-10-11 21:49:53 +0300

    sixth task

---
+++ src/tasks_java/sixth.java
@@ -0,0 +1,34 @@
+package tasks_java;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class sixth{
+	public static void main(String args[]) throws IOException{
+		BufferedReader reader = new BufferedReader(
+	            new InputStreamReader
+	            (new FileInputStream
+	            ("C:\\Users\\Anastasia\\Desktop\\openmp.txt")));
+		char c=args[0].charAt(0);
+		int number = CountSymbol(reader, c);
+	System.out.println(c+": "+number);
+	}
+	
+	
+	public static int CountSymbol(BufferedReader reader, char c) throws IOException{
+		 int result=0;
+		 String line;
+         while ((line = reader.readLine()) != null){
+	         for(char element : line.toCharArray()){
+	        	 if (element == c) result++;
+	         }
+         }
+         return result;
+	}
+}
+		
+			
+
+
