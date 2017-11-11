4eca647a57e91d3474fae107c9121a635ba8fa67
nastenka-28-10 <nastenka28.10.96@gmail.com>
2017-11-11 09:51:56 +0300

    redesigned for comparison with rem, realized function rem

--- src/tasks_java/tasks_java.java
+++ src/tasks_java/tasks_java.java
@@ -3,22 +3,36 @@ import java.lang.Math;
 
 public class tasks_java {
 	public static void main(String args[]){
-		System.out.println("gcd by %: "+gcd_euclid(-20,42)+"\n");
-		System.out.println("gcd by floorMod: "+gcd_euclid_floorMod(-20,42)+"\n");
+		System.out.println("gcd by %: "+gcd_euclid(20,-42)+"\n");
+		System.out.println("gcd by floorMod: "+gcd_euclid_floorMod(20,-42)+"\n");
+		System.out.println("gcd by rem: "+gcd_euclid_rem(20,-42)+"\n");
 		
 	}
 	public static int gcd_euclid(int a, int b){
-		while (a!=0 && b!=0) {
-			 if (Math.abs(a)>Math.abs(b)) a %= b; else b %= a;
-			}
-		return (Math.abs(a)+Math.abs(b));
+		if(b == 0) return a;
+		return gcd_euclid(b, a%b);
 	}
+	
 	public static int gcd_euclid_floorMod(int a, int b){
-		while (a!=0 && b!=0) {
-			 if (Math.abs(a)>Math.abs(b)) a=Math.floorMod(Math.abs(a),Math.abs(b)); else b=Math.floorMod(Math.abs(b),Math.abs(a));
-			}
-		return (a+b);
-		
+		if(b == 0) return a;
+		return gcd_euclid_floorMod(b, Math.floorMod(a, b) );	
+	}
+	
+	public static int gcd_euclid_rem(int a, int b){
+		if(b == 0) return a;
+		return gcd_euclid_rem(b, rem(a, b));
+	}
+	
+	
+	public static int rem(int a, int b){
+        if (b < 0) {
+            return rem(a, -b);
+        }
+        if (a < 0) {
+            if (rem(-a, b) != 0)
+            return b - rem(-a, b);
+        }
+        return a%b;
 	}
 
 }	
